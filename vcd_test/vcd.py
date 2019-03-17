27dc076d50667d555f416d34eac5bdc805627532
edgykuma <edric.kusuma@gmail.com>
2019-03-16 23:56:29 -0400

    made vcd dict dump prettier

--- vcd_test/vcd.py
+++ vcd_test/vcd.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 
 import Verilog_VCD as vcd
+import pprint
 
-file1 = "ham_dump1.vcd"
-file2 = "ham_dump2.vcd"
+#file1 = "ham_dump1.vcd"
+#file2 = "ham_dump2.vcd"
+
+file1 = "test1.vcd"
+file2 = "test2.vcd"
 
 parsed1 = vcd.parse_vcd(file1)
 parsed2 = vcd.parse_vcd(file2)
 
 correct = parsed1 == parsed2
 print(correct)
-#if not correct:
-#    print("file1:")
-#    print(parsed1)
-#    print("file2:")
-#    print(parsed2)
+if not correct:
+    pprint.pprint("file1:")
+    pprint.pprint(parsed1)
+    pprint.pprint("file2:")
+    pprint.pprint(parsed2)
